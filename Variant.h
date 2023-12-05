d33a07c7372f091b549e309962a6ca0054aaca93
Binhao <146821720+PaxuitoGIT@users.noreply.github.com>
2023-12-05 10:10:05 +0000

    fix variant.h

---
+++ Variant.h
@@ -0,0 +1,21 @@
+#ifndef VARIANT_H
+#define VARIANT_H
+
+#include <string>
+#include <variant>
+using namespace std;
+
+class Variant {
+    private:
+    variant<int, float, string> value;
+    public:
+    Variant(int v);
+    Variant(float v);
+    Variant(const string& v);
+
+    int getInt() const;
+    float getFloat() const;
+    string getString() const;
+};
+
+#endif
