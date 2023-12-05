d5496391d4b48d4a5ed9c19341a78fb04f546e6e
Binhao <146821720+PaxuitoGIT@users.noreply.github.com>
2023-12-05 09:54:13 +0000

    lookup en cpp

--- environment.cpp
+++ environment.cpp
@@ -5,3 +5,10 @@ environment::environment() {}
 void environment::insert(const std::string& symbol, const std::string& value) {
     env[symbol] = value;
 }
+
+std::string environment::lookup(const std::string& symbol) {
+    if (env.find(symbol) == env.end()) {
+        return env[symbol];
+    }
+    return env[symbol];
+}
