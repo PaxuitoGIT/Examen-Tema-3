5b5ecf64ffd58663cf71e12859715378b64c3fbf
Binhao <146821720+PaxuitoGIT@users.noreply.github.com>
2023-12-05 10:13:25 +0000

    Variant int

--- Variant.cpp
+++ Variant.cpp
@@ -4,3 +4,10 @@
 Variant::Variant(int v) : value(v) {}
 Variant::Variant(float v) : value(v) {}
 Variant::Variant(const string& v) : value(v) {}
+
+int Variant::getInt() const {
+    if (std::holds_alternative<int>(value)) {
+        return std::get<int>(value);
+    }
+    throw std::runtime_error("No es un entero");
+}
